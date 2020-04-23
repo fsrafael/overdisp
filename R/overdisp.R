c7e07a399dd906742e22c5213e788ac9a65f7e8a
Rafael de Freitas Souza <rafael@unemat.br>
2020-04-23 00:04:39 -0300

    Overdisp new version
    - Corrige o problema do uso de caracteres especiais como nomes de variáveis

--- R/overdisp.R
+++ R/overdisp.R
@@ -2,6 +2,11 @@ overdisp <- function(x, dependent.position = NULL, predictor.position = NULL) {
   data.name <- base::deparse(substitute(x))
   dependent.name <- base::names(x)[dependent.position]
   predictors.name <- base::names(x)[predictor.position]
+  var.names <- c(dependent.name, predictors.name)
+  if(base::sum(base::as.numeric(base::grepl("[-+!#$%&*,/:;<=>?@^`|~]", base::names(var.names)))) > 0){
+    base::print("Please, remove all following characters like -+!#$%&*,/:;<=>?@^`|~ and quotation marks from the names of the selected variables. Then run overdisp again")
+    stop()
+  }
   f <- stats::as.formula(base::paste(dependent.name,sep = "~",base::paste(predictors.name,collapse="+")))
   model <- stats::glm(f, x, family = "poisson")
   step_model <- stats::step(model, trace = 0)
@@ -11,7 +16,7 @@ overdisp <- function(x, dependent.position = NULL, predictor.position = NULL) {
   summary(aux.model)
   summary.amodel <- base::summary(aux.model)
   results <- base::list(statistic=c("Lambda t test score:"=summary.amodel$coefficients[3]),
-                        p.value=summary.amodel$coefficients[4], 
+                        p.value=summary.amodel$coefficients[4],
                         method="Overdispersion Test - Cameron & Trivedi (1990)",
                         data.name=data.name,alternative = "overdispersion if lambda p-value is less than or equal to the stipulated significance level")
   base::class(results) <- "htest"
